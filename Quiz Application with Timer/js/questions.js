5c829978a3c9849d771c0f195d5c653eda7fc032
jeffjunho11 <jeffjunho11@naver.com>
2022-05-24 22:06:23 +0900

    quiz application add

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,36 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "유네스코란?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "국제 연합 기관 중 하나",
+      "시민단체",
+      "",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+];
